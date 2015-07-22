10cc7321e473e66e1a7d1c8eb1f169a1950e24c2
Carolina <krito_j_g@utp.edu.co>
2015-07-22 21:07:41 +0000

    Suma matrices cuadradas en CUDA

--- Suma_Matrices_CUDA.cu
+++ Suma_Matrices_CUDA.cu
@@ -1,92 +1,70 @@
-//PROGRAMA QUE SUMA DOS MATRICES EN C++
+//PROGRAMA QUE SUMA DOS MATRICES EN CUDA
 #include<iostream>
+#include<stdio.h>
 #include<malloc.h>
 #include<cuda.h>
 using namespace std; 
 
 
-__global__ void SumaMatricesCU(int** A,int** B,int** C,int x,int y){
-	int idx=blockIdx.x*blockDim.x + threadIdx.x;//columnas
-	int idy=blockIdx.y*blockDim.y + threadIdx.y;//filas
-	if((idx<x)&&(idy<y)){
-		C[idx][idy]=A[idx][idy]+B[idx][idy];
+__global__ void SumaMatricesCU(int* A,int* B,int* C,int m,int n){//matriz[m][n]
+	int row = blockIdx.y*blockDim.y + threadIdx.y;
+	int col = blockIdx.x*blockDim.x + threadIdx.x;
+	int i=row*m+col;
+	if((row<m)&&(col<n)){
+		C[i]=A[i]+B[i];
 	}	
 }
 
-void imprimeMatriz(int** A, int x, int y){
-	for(int i=0;i<x;i++){
-		for(int j=0;j<y;j++){
-			cout<<A[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-
-}
-
-void inicializaMatriz(int** X,int filas, int columnas){//la llena de ceros
-	for(int i=0;i<filas;i++){
-		for(int j=0;j<columnas;j++){
-			X[i][j]=1;
-		}
-	}
-
-}
+void imprime(int* A,int x, int y){
+	for(int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
+            cout<<A[(i*x)+j];
+        }
+        cout<<endl;
+    }
+}	
 
 int main(void){
-	int **A,**B,**C,**d_A,**d_B,**d_C,x=2000,y=1500;
-	A=(int**)malloc(x*sizeof(int*));//reservamos memoria
-	for(int i=0;i<x;i++){
-		A[i]=(int*)malloc(y*sizeof(int*));
-	}
-	B=(int**)malloc(x*sizeof(int*));//reservamos memoria
-	for(int i=0;i<x;i++){
-		B[i]=(int*)malloc(y*sizeof(int*));
-	}
-	C=(int**)malloc(x*sizeof(int*));//reservamos memoria
-	for(int i=0;i<x;i++){
-		C[i]=(int*)malloc(y*sizeof(int*));
+	int x=2048,y=2048,SIZE=x*y;//dimensiones de la matriz	x = columnas, y = filas
+	int *d_A,*d_B,*d_C,*A,*B,*C;
+
+	//reservamos memoria
+	A=(int*)malloc(SIZE*sizeof(int));//matriz cuadrada
+	B=(int*)malloc(SIZE*sizeof(int));
+	C=(int*)malloc(SIZE*sizeof(int));
+	cudaMalloc((void**)&d_A,SIZE*sizeof(int));
+	cudaMalloc((void**)&d_B,SIZE*sizeof(int));
+	cudaMalloc((void**)&d_C,SIZE*sizeof(int));
+	
+	//inicializa las matrices A y B
+	for(int i=0;i<SIZE;i++){
+			A[i]=1;
+			B[i]=1;
 	}
 
-	cudaMalloc(&d_A,x*sizeof(int));
-	for(int i=0;i<x;i++){
-		cudaMalloc(&d_A[i],y*sizeof(int));
-	}
-	cudaMalloc(&d_B,x*sizeof(int));
-	for(int i=0;i<x;i++){
-		cudaMalloc(&d_B[i],y*sizeof(int));
-	}
-	cudaMalloc(&d_C,x*sizeof(int));
-	for(int i=0;i<x;i++){
-		cudaMalloc(&d_C[i],y*sizeof(int));
-	}
-	
-	inicializaMatriz(A,x,y);
-	inicializaMatriz(B,x,y);
+	cudaMemcpy(d_A,A,SIZE*sizeof(int),cudaMemcpyHostToDevice);//destino d_A y origen A
+	cudaMemcpy(d_B,B,SIZE*sizeof(int),cudaMemcpyHostToDevice);
 
-	cudaMemcpy(&d_A,A,x*y*sizeof(int),cudaMemcpyHostToDevice);//destino d_A y origen A
-	cudaMemcpy(&d_B,B,x*y*sizeof(int),cudaMemcpyHostToDevice);
-	
-	//47*63*1024=3032064  esta es la cantidad de hilos que vamos a utilizar para hacer la suma de las matrices
-	//porque las matrices tienen una dimensión de 2000*1500=3000000 
-	//32*32 = 1024 hilos en cada bloque
-	//2000/32=63, 1500/32=47
 	dim3 dimblock(32,32,1);//dimensión de los bloques(cantidad de hilos que se van a utilizar)
 	dim3 dimGrid(ceil(x/32),ceil(y/32),1);//dimensión de la malla (cantidad de bloques que se van a utilizar)
 	
-	SumaMatricesCU<<<dimGrid,dimblock>>>(d_A,d_B,d_C,x,y);//enviamos x y y porque son las dimensiones de la matriz y son menos 
+	SumaMatricesCU<<<dimGrid,dimblock>>>(d_A,d_B,d_C,y,x);//enviamos x y y porque son las dimensiones de la matriz y son menos 
 	//de los hilos que se van a utilizar
 
-	cudaDeviceSynchronize();//espera que termine la funcion anterior 
-	cudaMemcpy(C,d_C,x*y*sizeof(int),cudaMemcpyDeviceToHost);//copia la operacion relizada en el device al host en el vector C
+	cudaDeviceSynchronize();
 
-	imprimeMatriz(C,x,y);
+	cudaMemcpy(C,d_C,SIZE*sizeof(int),cudaMemcpyDeviceToHost);
 	
-	free(*A);free(*B);free(*C);free(A);free(B);free(C);
+	imprime(C,x,y);
+
+	cout<<endl;
+	
+	free(A);free(B);free(C);
 	cudaFree(d_A);
 	cudaFree(d_B);
 	cudaFree(d_C);	
 	
-	
+
 	return 0;
 
 }
